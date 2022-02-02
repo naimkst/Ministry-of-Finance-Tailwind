58f9e0fc4dc342a7acbad793e80db09c850d4a42
gobindasarkaritech <gobindasarkar@itech-softsolutions.com>
2022-02-02 19:39:08 +0600

    first commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,79 @@
+(function($) {
+  "use strict";
+
+  
+  jQuery(document).ready(function(){
+    /*-------------------------------------------
+    js scrollup
+    --------------------------------------------- */
+    $.scrollUp({
+      scrollText: '<i class="fa fa-angle-up"></i>',
+      easingType: 'linear',
+      scrollSpeed: 300,
+      animation: 'fade'
+    }); 
+    /*-------------------------------------------
+    hero-slider active
+    --------------------------------------------- */
+    $('.hero-slider').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      focusOnSelect: true,
+      dots: false,
+      arrows: true,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+    });
+    /*-------------------------------------------
+    service-slide active
+    --------------------------------------------- */
+    $('.service-slide').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      focusOnSelect: true,
+      dots: true,
+      arrows: false,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    /*-------------------------------------------
+    js counterUp
+    --------------------------------------------- */
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+    });
+  });
+
+})(jQuery);
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#D1B129',
+      'hover': '#F4CA14',
+      'bcolor': '#7A7A7A',
+      'color-one': '#08173D',
+      'color-two': '#020E2E',
+      'bg-one': '#08173D',
+      'bg-two': '#041D5C',
+      'white': '#ffffff',
+      'body-bg': '#f8f8f8',
+      'transparent': "transparent"
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      nsa: ['Noto Sans Arabic', 'sans-serif'],
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'active'],
+    textColor: ['responsive', 'hover', 'active'],
+  },
+  plugins: [],
+}

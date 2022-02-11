8fd9a9ef36f3aa1d7d9a6908d1da4455ecdf8e55
gobindasarkaritech <gobindasarkar@itech-softsolutions.com>
2022-02-11 11:49:22 +0600

    slick slider rtl problem solve

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -21,58 +21,121 @@
     /*-------------------------------------------
     hero-slider active
     --------------------------------------------- */
-    $('.hero-slider').slick({
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      focusOnSelect: true,
-      dots: false,
-      arrows: true,
-      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
-      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
-    });
+    
+    (function () {
+      if( $( "html" ).attr("dir") == "rtl" ) {   
+        $('.hero-slider').slick({
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          focusOnSelect: true,
+          dots: false,
+          arrows: true,
+          rtl: true,
+          prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+          nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+        });   
+      }else {
+        $('.hero-slider').slick({
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          focusOnSelect: true,
+          dots: false,
+          arrows: true,
+          prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+          nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+        });
+      }
+    })();
     /*-------------------------------------------
     service-slide active
     --------------------------------------------- */
-    $('.service-slide').slick({
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      focusOnSelect: true,
-      dots: true,
-      arrows: false,
-      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
-      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    });
+    (function () {
+      if( $( "html" ).attr("dir") == "rtl" ) {   
+        $('.service-slide').slick({
+          infinite: true,
+          speed: 500,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          focusOnSelect: true,
+          dots: true,
+          arrows: false,
+          rtl: true,
+          prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+          nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });  
+      }else {
+        $('.service-slide').slick({
+          infinite: true,
+          speed: 500,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          focusOnSelect: true,
+          dots: true,
+          arrows: false,
+          prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+          nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+      }
+    })();
+    
     /*-------------------------------------------
     js counterUp
     --------------------------------------------- */
